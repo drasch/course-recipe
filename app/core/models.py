7fe5c98e66ecf2354772136925db852f8027b5b6
David Rasch <david.rasch@infiniaml.com>
2020-03-26 15:28:18 -0400

    User: add superuser function

--- app/core/models.py
+++ app/core/models.py
@@ -16,6 +16,16 @@ class UserManager(BaseUserManager):
 
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email: str, password: str, **extra_fields):
+        """Creates and saves a new superuser"""
+
+        user = self.create_user(email, password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
         user.save()
 
         return user
