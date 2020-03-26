ecf965c68e4c23fa80a9fddb117e484ab8c20d6f
David Rasch <david.rasch@infiniaml.com>
2020-03-26 16:03:56 -0400

    Admin: starting admin registration

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,29 @@
+from django.test import Client, TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        """Setup some seed data for testing"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='password333'
+        )
+
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@example.com',
+            password='lesssecure123',
+            name='Test user full name'
+        )
+
+    def test_users_listed(self):
+        """Tests that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
